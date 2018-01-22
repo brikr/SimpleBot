7c39eb39a6352ffdcafc05e017065e319bc685eb
Zoey <joeydesautels@gmail.com>
2018-01-22 11:58:00 -0500

    Fixed streams not being notified.

--- modules/streams.js
+++ modules/streams.js
@@ -23,29 +23,32 @@ function streams(bot) {
 	function twitchRequest() {
 		request.get(requestOptions, function(err, response, body) {
 			if(!err) { // Make sure the request went well
-				try { // Shitty try/catch here, the bot may sometimes crash from this and who the fuck knows why
-					const stream = JSON.parse(body).stream;
+				var stream = null;
 
-					// Make sure a stream is on
-					if(stream != null) {
-						if(lastStreamID != stream.id) { // Make sure this stream hasn't been announced yet
-							var streamEmbed = simplebot.createEmbed();
-
-							streamEmbed.addField("Now Playing", stream.game);
-							streamEmbed.addField("Stream Title", stream.channel.status);
-							streamEmbed.addField("Followers", stream.channel.followers.toLocaleString('en'), true);
-							streamEmbed.addField("Total Views", stream.channel.views.toLocaleString('en'), true);
-							streamEmbed.setThumbnail(stream.channel.logo);
-							streamEmbed.setFooter("Stream started at");
-							streamEmbed.setTimestamp(stream.created_at);
-
-							// At least, send the notification
-							bot.guild.channels.get(bot.channels.notification).send("<@&344292325423316992>\n" + stream.channel.display_name + " just went live!\nWatch the stream at " + stream.channel.url, streamEmbed).then(() => {
-								lastStreamID = stream.id; // Make sure it doesn't post it twice
-							});
-						}
-					}
+				// Sometimes the code would crash due to bad parsing here
+				try {
+					stream = JSON.parse(body).stream;
 				} catch(e) {}
+
+				// Make sure a stream is on
+				if(stream != null) {
+					if(lastStreamID != stream.id) { // Make sure this stream hasn't been announced yet
+						var streamEmbed = simplebot.createEmbed();
+
+						streamEmbed.addField("Now Playing", stream.game);
+						streamEmbed.addField("Stream Title", stream.channel.status);
+						streamEmbed.addField("Followers", stream.channel.followers.toLocaleString('en'), true);
+						streamEmbed.addField("Total Views", stream.channel.views.toLocaleString('en'), true);
+						streamEmbed.setThumbnail(stream.channel.logo);
+						streamEmbed.setFooter("Stream started at");
+						streamEmbed.setTimestamp(stream.created_at);
+
+						// At last, send the notification
+						bot.guild.channels.get(bot.channels.notification).send("<@&344292325423316992>\n" + stream.channel.display_name + " just went live!\nWatch the stream at " + stream.channel.url, streamEmbed).then(() => {
+							lastStreamID = stream.id; // Make sure it doesn't post it twice
+						});
+					}
+				}
 			} 
 		});
 	}
